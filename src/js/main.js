aa8db33ee65d5ccca547f2e70a9a66414f36be77
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2024-11-14 10:11:13 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+//Eingabedaten holen
+let titel_1 = prompt ("Titel 1");
+console.log(titel_1);
+let typ_1 = prompt ("Typ {Einahmen und Ausgaben:}");
+console.log(typ_1);
+let betrag_1 = prompt ("Betrag {in Cent:}");  // um keine Kommazahlen zu verwenden
+console.log(betrag_1);
+let datum_1 = prompt ("Datum {JJJJ-MM-TT:}");
+console.log(datum_1);
+
+//Gesamtbilanz 
+let einnahmen;
+let ausgaben;
+let bilanz;
