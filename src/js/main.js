cc19f97f8c47f97e30c574451d0a24a7db62ac43
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2025-04-21 12:54:43 +0200

    Abschnitt 32:Teil 14:Projekt die Pfeil-funkt. einfügen

--- src/js/main.js
+++ src/js/main.js
@@ -8,24 +8,14 @@ const haushaltsBuch = {
   eintrag_erfassen() {
     let neuerEintrag = new Map();
     const titelInput = prompt("Titel:");
-    neuerEintrag.set(
-      "titel",
-      this.titelVerarbeiten(titelInput ? titelInput.trim() : "")
-    );
+    neuerEintrag.set("titel",this.titelVerarbeiten(titelInput ? titelInput.trim() : ""));
     const typInput = prompt("Typ (Einahmen und Ausgaben:)");
-    neuerEintrag.set(
-      "typ",
-      this.typVerarbeiten(typInput ? typInput.trim() : "")
-    );
+    neuerEintrag.set("typ",this.typVerarbeiten(typInput ? typInput.trim() : ""));
     const betragInput = prompt("Betrag (in Euro, ohne €-Zeichen:)");
-    neuerEintrag.set(
-      "betrag",
-      this.betragVerarbeiten(betragInput ? betragInput.trim() : "")
+    neuerEintrag.set("betrag", this.betragVerarbeiten(betragInput ? betragInput.trim() : "")
     );
     const datumInput = prompt("Datum (JJJJ-MM-TT:)");
-    neuerEintrag.set(
-      "datum",
-      this.datumVerarbeiten(datumInput ? datumInput.trim() : "")
+    neuerEintrag.set( "datum", this.datumVerarbeiten(datumInput ? datumInput.trim() : "")
     );
     neuerEintrag.set("timespamp", Date.now());
 
@@ -33,9 +23,8 @@ const haushaltsBuch = {
       this.eintraege.push(neuerEintrag);
     } else {
       console.log("Folgenden Fehler wurden gefunden:");
-      this.fehler.forEach(function (fehler) {
-        console.log(fehler);
-      });
+      this.fehler.forEach(fehler => 
+        console.log(fehler));
     }
   },
 
@@ -108,7 +97,7 @@ const haushaltsBuch = {
   },
 
   eintraege_sortieren() {
-    this.eintraege.sort(function (eintrag_a, eintrag_b) {
+    this.eintraege.sort((eintrag_a, eintrag_b) => {
       if (eintrag_a.get("datum") > eintrag_b.get("datum")) {
         return -1;
       } else if (eintrag_a.get("datum") < eintrag_b.get("datum")) {
@@ -120,83 +109,45 @@ const haushaltsBuch = {
   },
 
   html_eintrag_generieren(eintrag) {
-    //li erstellen
     let listenpunkt = document.createElement("li");
-    //Klasse setzen
     if (eintrag.get("typ") === "einnahme") {
       listenpunkt.setAttribute("class", "einnahme");
     } else if (eintrag.get("typ") === "ausgabe") {
       listenpunkt.setAttribute("class", "ausgabe");
     }
-    listenpunkt.setAttribute("data-timestamp", eintrag.get("timestamp")); //Datum erstellen
-    //span für Datum erstellen
+    listenpunkt.setAttribute("data-timestamp", eintrag.get("timestamp")); 
     let datum = document.createElement("span");
-    //Klasse fürs Datum  setzen
     datum.setAttribute("class", "datum");
-    // eintragstyp setzen
     datum.textContent = eintrag.get("datum").toLocaleDateString("de-DE", {
       year: "numeric",
       month: "2-digit",
       day: "2-digit",
     });
-    //Datum in den li einsetzen
     listenpunkt.insertAdjacentElement("afterbegin", datum);
-
-    //span für Titel erstellen
     let titel = document.createElement("span");
-    //klasse setzen
     titel.setAttribute("class", "titel");
-    //Textcontent setzen
     titel.textContent = eintrag.get("titel");
-    //Titel span einsetzen
     datum.insertAdjacentElement("afterend", titel);
-
-    //span für Betrag erstellen
     let betrag = document.createElement("span");
-    //klasse setzen
     betrag.setAttribute("class", "betrag");
-    //Textcontent setzen
     betrag.textContent = `${(eintrag.get("betrag") / 100).toFixed(2).replace(/\./, ",")} €`;
-    //Betrag einsetzen
     titel.insertAdjacentElement("afterend", betrag);
-
-    // Button erstellen
     let button = document.createElement("button");
-    //klasse für button setzen
     button.setAttribute("class", "entfernen-button");
-    //Textcontent für button setzen
     betrag.insertAdjacentElement("afterend", button);
-
-    //Icon erstellen
     let icon = document.createElement("i");
-    //klasse für icon setzen
     icon.setAttribute("class", "fas fa-trash");
-    //Icon einsetzen
     button.insertAdjacentElement("afterbegin", icon);
     return listenpunkt;
   },
 
   eintraege_anzeigen() {
-    //überprüfen ob eine ul vorhanden ist, ggf. ul entfernen
-    document
-      .querySelectorAll("monaltsliste ul")
-      .forEach(function (eintragsliste) {
-        eintragsliste.remove();
-      });
-    //<ul> erstellen
+    document .querySelectorAll("monaltsliste ul").forEach(eintragsliste=>
+      eintragsliste.remove());
     let eintragsliste = document.createElement("ul");
-    //über eintraege[]iterierern
-    for (let eintrag of this.eintraege) {
-      // für jeden Eintrag einen HTML-Eintrag erstellen,HTML Eintrag in die <ul> einsetzen
-      eintragsliste.insertAdjacentElement(
-        "beforeend",
-        this.html_eintrag_generieren(eintrag)
-      );
-    }
-    // <ul>in den article.monatsliste einsetzen
-    document
-      .querySelector(".monatsliste")
-      .insertAdjacentElement("afterbegin", eintragsliste);
+    this.eintraege.forEach(eintrag =>
+      eintragsliste.insertAdjacentElement("beforeend",this.html_eintrag_generieren(eintrag)));
+    document .querySelector(".monatsliste").insertAdjacentElement("afterbegin", eintragsliste);
   },
 
   gesamtbilanz_erstellen() {
@@ -205,7 +156,7 @@ const haushaltsBuch = {
     neue_gesamtBilanz.set("ausgaben", 0);
     neue_gesamtBilanz.set("bilanz", 0);
 
-    this.eintraege.forEach(function (eintrag) {
+    this.eintraege.forEach (eintrag => {
       switch (eintrag.get("typ")) {
         case "einahme":
           neue_gesamtBilanz.set(
@@ -235,68 +186,33 @@ const haushaltsBuch = {
     this.gesamtBilanz = neue_gesamtBilanz;
   },
 
-  // <aside id="gesamtbilanz">
-  //       <h1>Gesamtbilanz</h1>
-  //       <div class="gesamtbilanz-zeile einnahmen"><span>Einnahmen:</span><span>4228,74€</span></div>
-  //       <div class="gesamtbilanz-zeile ausgaben"><span>Ausgaben:</span><span>2988,88€</span></div>
-  //       <div class="gesamtbilanz-zeile bilanz"><span>Bilanz:</span><span class="positiv">1239,86€</span></div>
-  //   </aside>
-
-  //html gesamtbilanz generieren
   html_gesamtbilanz_generieren() {
-    //anhand der aktuellen gesamzbilanz, die gesamtbilanz neu generieren
     let gesamtbilanz = document.createElement("aside");
-    //id setzen
     gesamtbilanz.setAttribute("id", "gesamtbilanz");
-    //h1 erstellen
     let h1 = document.createElement("h1");
-    //Textcontent setzen
     ueberschrift.textContent = "Gesamtbilanz";
-    //h1 in die aside einsetzen
     gesamtbilanz.insertAdjacentElement("afterbegin", ueberschrift);
 
-    //ERSTER DIV für Einnahmen erstellen
     let einnahmen_zeile = document.createElement("div");
-    //klasse setzen
     einnahmen_zeile.setAttribute("class", "gesamtbilanz-zeile einnahmen");
-    //span für Einnahmen erstellen
     let einnahmen_titel = document.createElement("span");
-    //Textcontent setzen
     einnahmen_titel.textContent = "Einnahmen:";
-    //span in die div einsetzen
     einnahmen_zeile.insertAdjacentElement("afterbegin", einnahmen_titel);
-
-    //span für Betrag erstellen
     let einnahmen_betrag = document.createElement("span");
-    //Textcontent setzen
     einnahmen_betrag.textContent = `${(this.gesamtbilanz.get("einnahmen")/100).toFixed(2).replace(/\./, ",")} €`;
-    //span in die div einsetzen
     einnahmen_zeile.insertAdjacentElement("beforend", einnahmen_betrag);
-    //div in die aside einsetzen
     gesamtbilanz.insertAdjacentElement("beforeend", einnahmen_zeile);
 
-    //ZWEITES DIV für Ausgaben erstellen
-     //div für Einnahmen erstellen
      let ausgaben_zeile = document.createElement("div");
-     //klasse setzen
      ausgaben_zeile.setAttribute("class", "gesamtbilanz-zeile ausgaben");
-     //span für Einnahmen erstellen
      let ausgaben_titel = document.createElement("span");
-     //Textcontent setzen
      ausgaben_titel.textContent = "Ausgaben:";
-     //span in die div einsetzen
-     ausgaben_zeile.insertAdjacentElement("afterbegin", ausgaben_titel);
-     //span für Betrag erstellen
+     ausgaben_zeile.insertAdjacentElement("afterbegin", ausgaben_titel);  
      let ausgaben_betrag = document.createElement("span");
-     //Textcontent setzen
      ausgaben_betrag.textContent = `${(this.gesamtbilanz.get("ausgaben")/100).toFixed(2).replace(/\./, ",")} €`;
-     //span in die div einsetzen
      ausgaben_zeile.insertAdjacentElement("beforend", ausgaben_betrag);
-     //div in die aside einsetzen
      gesamtbilanz.insertAdjacentElement("beforeend", ausgaben_zeile);
     
-     // DRITTES DIV für Bilanz erstellen
-     //div für Bilanz erstellen
      let bilanz_zeile = document.createElement("div");
      bilanz_zeile.setAttribute("class", "gesamtbilanz-zeile bilanz");
      let bilanz_titel = document.createElement("span");
@@ -308,6 +224,7 @@ const haushaltsBuch = {
       } else if(this.gesamtbilanz.get("bilanz") < 0) {
         bilanz_betrag.setAttribute("class", "negativ");
       }
+
      bilanz_betrag.textContent = `${(this.gesamtbilanz.get("bilanz")/100).toFixed(2).replace(/\./, ",")} €`;
      bilanz_zeile.insertAdjacentElement("beforend", bilanz_betrag);
      gesamtbilanz.insertAdjacentElement("beforeend", bilanz_zeile);
@@ -315,14 +232,9 @@ const haushaltsBuch = {
   },
 
   gesamtbilanz_anzeigen() {
-    //prüfen ob bereits Gesamtbilanz angezeigt wird,
-    document.querySelectorAll("#gesamtbilanz").forEach(function (gesamtbilanz) {
-      gesamtbilanz.remove(); // ggf. entfernen
-    });
-    // neue gesamtbilanz anzeigen (html_gesamtbilanz-generieren())
-    document
-      .querySelector("body")
-      .insertAdjacentElement("beforeend", this.html_gesamtbilanz_generieren());
+    document.querySelectorAll("#gesamtbilanz").forEach(gesamtbilanz => 
+      gesamtbilanz.remove());
+    document.querySelector("body") .insertAdjacentElement("beforeend", this.html_gesamtbilanz_generieren());
   },
 
   eintrag_hinzufuegen() {
@@ -335,7 +247,7 @@ const haushaltsBuch = {
         this.gesamtbilanz_erstellen();
         this.gesamtbilanz_anzeigen();
       } else {
-        this.fehler = []; // Fehler löschen
+        this.fehler = []; 
       }
       weiterer_eintrag = confirm("Weiteren Eintrag hinzufügen?");
     }
